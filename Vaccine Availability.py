a3b60ec5394b6797f14aae758131e4ac7ff7f64a
Anup Ray <43163573+anupray95@users.noreply.github.com>
2021-05-11 15:09:15 +0530

    Fixed Get api bug .

--- Vaccine Availability.py
+++ Vaccine Availability.py
@@ -6,7 +6,8 @@ import datetime
 
 
 payload = ""
-headers = {}
+headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36'}
+
 
 
 # Enter State and District Name
